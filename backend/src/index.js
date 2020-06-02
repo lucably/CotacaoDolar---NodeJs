88859c79f613ea9ca04da364b506504505f3a103
Lucas Rodrigues Medeiros <lucas.medeiros@radixeng.com.br>
2020-06-02 15:04:53 -0300

    Funcoes implementadas

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,7 +3,7 @@ const app = express();
 const api = require('./services/index');
 
 
-function urlBase() {
+const urlBase = () => {
 
     return api.get(api.baseURL).then(function (resposta) {
         
@@ -14,16 +14,34 @@ function urlBase() {
 
 }
 
-function getCoinsBetweenDate(){
-    const usd = 'USD';
-    const dateI ='11-11-2017';
-    const dateF ='12-12-2018';
+const getCoinsBetweenDate = () =>{
+    
+    const dateI ='11-11-2017'
+    const dateF ='12-12-2018'
 
-    return api.get(`https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/CotacaoDolarPeriodo(dataInicial=@dataInicial,dataFinalCotacao=@dataFinalCotacao)?@dataInicial='11-11-2017'&@dataFinalCotacao='12-12-2018'&$top=100&$format=json`).then(function (resposta) {
-        
+    return api.get(`https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/CotacaoDolarPeriodo(dataInicial=@dataInicial,dataFinalCotacao=@dataFinalCotacao)?@dataInicial='${dateI}'&@dataFinalCotacao='${dateF}'&$top=100&$format=json`).then(function (resposta) {
         return resposta.data;
     }).catch(function (error) {
-        console.log(error)
+        console.log(error);
+    })
+
+}
+
+const getDolarLessDays = () => {
+
+    const date = '11-27-2018'
+
+    const data = new Date();
+
+    const myData = '0'+(1+data.getMonth())+'-0'+data.getDate()+'-'+data.getFullYear();
+
+    console.log('Data: ',data);
+
+
+    return api.get(`https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/CotacaoDolarDia(dataCotacao=@dataCotacao)?@dataCotacao='${myData}'&$top=100&$format=json`).then(function (resposta) {
+        return resposta.data;
+    }).catch(function (error) {
+        console.log(error);
     })
 
 }
@@ -39,8 +57,16 @@ app.get('/', (req, res) => {
 
 });
 
-app.get('/moeda', (req, res) => {
+app.get('/moedaBetween', (req, res) => {
     getCoinsBetweenDate().then(data => {
+        res.json({
+           data
+        })
+    })
+})
+
+app.get('/moeda', (req, res) => {
+    getDolarLessDays().then(data => {
         res.json({
            data
         })
